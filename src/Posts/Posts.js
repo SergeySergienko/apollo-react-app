a70939149f53b599f69219d477e866757f8ea0b1
SergeySergienko <serg.v.sergienko@gmail.com>
2019-02-17 11:26:19 +0200

    Added Sorting & Pagination

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import { Link } from "react-router-dom";
@@ -6,30 +6,49 @@ import { Link } from "react-router-dom";
 export default class Posts extends Component {
   render() {
     return (
-      <ul>
+      <ol>
         <Query query={POSTS_QUERY}>
-          {({ loading, data }) => {
+          {({ loading, data, fetchMore }) => {
             return loading ? (
               <h2>Loading...</h2>
             ) : (
-              data.posts.map(({ id, title }) => (
-                <li key={id}>
-                  <Link to={`/post/${id}`}>
-                    <h4>{title}</h4>
-                  </Link>
-                </li>
-              ))
+              <Fragment>
+                {data.posts.map(({ id, title }) => (
+                  <li key={id}>
+                    <Link to={`/post/${id}`}>
+                      <h4>{title}</h4>
+                    </Link>
+                  </li>
+                ))}
+                <button
+                  onClick={() =>
+                    fetchMore({
+                      variables: {
+                        skip: data.posts.length
+                      },
+                      updateQuery: (prev, { fetchMoreResult }) => {
+                        if (!fetchMoreResult) return prev;
+                        return Object.assign({}, prev, {
+                          posts: [...prev.posts, ...fetchMoreResult.posts]
+                        });
+                      }
+                    })
+                  }
+                >
+                  Load More
+                </button>
+              </Fragment>
             );
           }}
         </Query>
-      </ul>
+      </ol>
     );
   }
 }
 
 const POSTS_QUERY = gql`
-  query allPosts {
-    posts {
+  query allPosts($skip: Int) {
+    posts(orderBy: createdAt_DESC, first: 2, skip: $skip) {
       id
       title
       body
