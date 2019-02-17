9b47d2b36d5f98d8cf1f9de4dfae222f6eb18862
SergeySergienko <serg.v.sergienko@gmail.com>
2019-02-17 19:12:30 +0200

    Added Apollo Persistant Cache

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { Component, Fragment } from "react";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "./App.css";
 import Post from "./Posts/Post";
 import NewPost from "./Posts/NewPost";
 import Posts from "./Posts/Posts";
 import gql from "graphql-tag";
-
-const defaultState = {
-  isEditMode: false
-};
-
-const client = new ApolloClient({
-  uri: "https://api-euwest.graphcms.com/v1/cjrxo7f6o22ld01gtcjp4w85x/master",
-  clientState: {
-    defaults: defaultState,
-    resolvers: {}
-  }
-});
 
 // ------------------------------
 // const POSTS_QUERY = gql`
@@ -46,21 +32,19 @@ const client = new ApolloClient({
 export default class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <Router>
-          <Fragment>
-            <div className="links">
-              <Link to="/">Home</Link>
-              <Link to="/post/new">New Post</Link>
-            </div>
-            <Switch>
-              <Route exact path="/" component={Posts} />
-              <Route exact path="/post/new" component={NewPost} />
-              <Route path="/post/:id" component={Post} />
-            </Switch>
-          </Fragment>
-        </Router>
-      </ApolloProvider>
+      <Router>
+        <Fragment>
+          <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/post/new">New Post</Link>
+          </div>
+          <Switch>
+            <Route exact path="/" component={Posts} />
+            <Route exact path="/post/new" component={NewPost} />
+            <Route path="/post/:id" component={Post} />
+          </Switch>
+        </Fragment>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './App'
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import App from "./App";
 
-ReactDOM.render(
-    <App />,
-  document.getElementById("root")
-);
+const defaultState = {
+  isEditMode: false
+};
+
+const cache = new InMemoryCache();
+
+persistCache({
+  cache,
+  storage: window.localStorage
+}).then(() => {
+  const client = new ApolloClient({
+    cache,
+    uri: "https://api-euwest.graphcms.com/v1/cjrxo7f6o22ld01gtcjp4w85x/master",
+    clientState: {
+      defaults: defaultState,
+      resolvers: {}
+    }
+  });
+
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById("root")
+  );
+});
